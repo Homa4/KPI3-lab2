3ce94d4286e60be10b5323cf4e948d48da444781
Tymur <tn3996580@gmail.com>
2025-03-18 23:18:56 +0200

    fix

--- implementation.go
+++ implementation.go
@@ -1,56 +1,56 @@
-// package lab2
+package lab2
 
-// import (
-// 	"fmt"
-// 	"math"
-// 	"strconv"
-// 	"strings"
-// )
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
 
-// // PrefixToPostfix converts
-// func CalculatePrefix(expression string) (int, error) {
-// 	tokens := strings.Fields(expression)
-// 	var stack []int
-// 	if strings.TrimSpace(expression) == "" {
-// 		return 0, fmt.Errorf("empty expression")
-// 	}
+// PrefixToPostfix converts
+func CalculatePrefix(expression string) (int, error) {
+	tokens := strings.Fields(expression)
+	var stack []int
+	if strings.TrimSpace(expression) == "" {
+		return 0, fmt.Errorf("empty expression")
+	}
 
-// 	for i := len(tokens) - 1; i >= 0; i-- {
-// 		token := tokens[i]
+	for i := len(tokens) - 1; i >= 0; i-- {
+		token := tokens[i]
 
-// 		switch token {
-// 		case "+", "-", "*", "/", "^":
-// 			if len(stack) < 2 {
-// 				return 0, fmt.Errorf("invalid expression")
-// 			}
-// 			a, b := stack[len(stack)-1], stack[len(stack)-2]
-// 			stack = stack[:len(stack)-2]
-// 			var res int
-// 			switch token {
+		switch token {
+		case "+", "-", "*", "/", "^":
+			if len(stack) < 2 {
+				return 0, fmt.Errorf("invalid expression")
+			}
+			a, b := stack[len(stack)-1], stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			var res int
+			switch token {
 
-// 			case "^":
-// 				res = int(math.Pow(float64(a), float64(b)))
-// 			case "+":
-// 				res = a + b
-// 			case "-":
-// 				res = a - b
-// 			case "*":
-// 				res = a * b
-// 			case "/":
-// 				if b == 0 {
-// 					return 0, fmt.Errorf("division by zero")
-// 				}
-// 				res = a / b
-// 			}
-// 			stack = append(stack, res)
-// 		default:
-// 			num, err := strconv.Atoi(token)
-// 			if err != nil {
-// 				return 0, fmt.Errorf("invalid token: %s", token)
-// 			}
-// 			stack = append(stack, num)
-// 		}
-// 	}
+			case "^":
+				res = int(math.Pow(float64(a), float64(b)))
+			case "+":
+				res = a + b
+			case "-":
+				res = a - b
+			case "*":
+				res = a * b
+			case "/":
+				if b == 0 {
+					return 0, fmt.Errorf("division by zero")
+				}
+				res = a / b
+			}
+			stack = append(stack, res)
+		default:
+			num, err := strconv.Atoi(token)
+			if err != nil {
+				return 0, fmt.Errorf("invalid token: %s", token)
+			}
+			stack = append(stack, num)
+		}
+	}
 
-// 	return stack[0], nil
-// }
+	return stack[0], nil
+}
